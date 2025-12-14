61635403a2dec18b59ebe7715a78faccaa670295
Madhan R <madhanravi@gmail.com>
2025-12-14 20:57:14 +0530

    Bug Fixes

--- src/App.js
+++ src/App.js
@@ -14,16 +14,29 @@ import SnakeLadder from "./components/SnakeLadder";
 function App() {
   return (
     <div className="App">
-      {/* <UsersList /> */}
-      {/* <UserSort /> */}
-      {/* <Login /> */}
-      {/* <Stopwatch /> */}
-      {/* <StopWatchMine /> */}
-      {/* <Todo /> */}
-      {/* <CustomStepCounter /> */}
-      {/* <DynamicShopping /> */}
-      {/* <TrafficLight /> */}
-      <SnakeLadder />
+      <div style={{ marginRight: 10 + "em" }}>
+        <UsersList />
+      </div>
+      <div style={{ marginRight: 10 + "em" }}>
+        <UserSort />
+      </div>
+      <div style={{ marginRight: 10 + "em" }}>
+        <Login />
+      </div>
+      <div style={{ marginRight: 10 + "em" }}>
+        <StopWatchMine />
+      </div>
+      <div style={{ marginRight: 10 + "em" }}>
+        <Todo />
+      </div>
+      <div style={{ marginRight: 10 + "em" }}></div>
+      <div style={{ marginRight: 10 + "em" }}>
+        <DynamicShopping />
+      </div>
+      <div style={{ marginRight: 10 + "em" }}>
+        <TrafficLight />
+      </div>
+      <div style={{ marginRight: 10 + "em" }}>{/* <SnakeLadder /> */}</div>
     </div>
   );
 }
